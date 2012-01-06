c207fa4350c05e2ae312476a2545ce6a81a1cc6f
Jorge Luis Pérez <jolisper@gmail.com>
2012-01-06 18:43:00 -0300

    New module Command to handle command line

--- lib/metronome.rb
+++ lib/metronome.rb
@@ -1,5 +1,44 @@
-require "metronome/version"
+require 'metronome/version'
+require 'optparse'
+require 'rubygame'
 
 module Metronome
-  # Your code goes here...
+  
+  module Command
+    
+    # Parse option and start metronome
+    def self.run( args )
+      options = {}
+
+      OptionParser.new do |opts|
+        opts.on( '-t BPM', '--tempo BPM', /^[0-9]+/ ) do |bpm|
+          options[:bpm] = bpm
+        end
+      end.parse!( args )
+
+      start( options[:bpm] )
+    end
+
+    # Start metronome
+    def self.start( bpm )
+      s = Rubygame::Sound.load( File.expand_path("../../sounds/tap_short.wav", __FILE__) )
+
+      t = Thread.new do
+        puts 'press Ctrl+C to stop'
+        # main loop
+        loop do
+          s.play
+          sleep( 60 / bpm.to_f ) 
+        end
+      end
+
+      begin 
+        t.join
+      rescue Interrupt => e
+        puts "\nmetronome off"
+      end
+    end
+
+  end
+
 end
